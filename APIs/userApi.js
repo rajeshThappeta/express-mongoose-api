ebb21d7e45ee8f1fcb4fd548d71e0ea0ea7400e9
rajesh <rajesh.t7982@gmail.com>
2024-11-15 19:24:58 +0530

    user registration&login

--- APIs/userApi.js
+++ APIs/userApi.js
@@ -1,57 +1,116 @@
-const exp=require('express')
-const userApp=exp.Router();
-const expressAsyncHandler=require('express-async-handler')
-const mongoose=require('mongoose')
+const exp = require("express");
+const userApp = exp.Router();
+const expressAsyncHandler = require("express-async-handler");
+const UserModel=require('../models/userModel')
+const bcryptjs=require('bcryptjs')
+const jwt=require('jsonwebtoken')
 
 //add body parser middleware
-userApp.use(exp.json())
+userApp.use(exp.json());
 
-//create userSChema (name,age,email)
-const userSchema=new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
-    },
-    age:{
-        type:Number,
-        required:true,
-        min:20,
-        max:40
-    },
-    email:{
-        type:String,
-        required:true
-    }
-},{"strict":"throw"})
-
-//create model for userSchema
-const UserModel=mongoose.model('user',userSchema)
 
 
 //user api
-userApp.get("/users",expressAsyncHandler(async(req,res)=>{
+userApp.get(
+  "/users",
+  expressAsyncHandler(async (req, res) => {
     //get all users from users collection
-   let usersList=await  UserModel.find()
-   //send res
-   res.send({message:"all users",payload:usersList})
-}))
-
+    let usersList = await UserModel.find();
+    //send res
+    res.send({ message: "all users", payload: usersList });
+  })
+);
 
 //create new user
-userApp.post("/user",expressAsyncHandler(async(req,res)=>{
+userApp.post(
+  "/user",
+  expressAsyncHandler(async (req, res) => {
     //get new user object
-    const newUser=req.body;
+    const newUser = req.body;
+    //hash the password
+    const hashedPassword=await bcryptjs.hash(newUser.password,5)
+    //replace plain password with hashed password
+    newUser.password=hashedPassword;
     //create document for new user
-    const userDocument=new UserModel(newUser)
+    const userDocument = new UserModel(newUser);
     //console.log(userDocument)
     //save to db
-    let dbRes=await userDocument.save()
-    console.log(dbRes)
+    let dbRes = await userDocument.save();
+    console.log(dbRes);
     //send res
-    res.send({message:"New user created"})
+    res.send({ message: "New user created" });
+  })
+);
 
+//user login route
+userApp.post('/login',expressAsyncHandler(async(req,res)=>{
+
+    //get user cred obj from client
+    const userCredObj=req.body;
+    //verify username
+    let userInDB=await UserModel.findOne({username:userCredObj.username})
+    console.log(userInDB)
+    //if username not found
+    if(userInDB===null){
+      res.send({message:"Invalid username"})
+    }
+    //if user existed,compare passwords
+    else{
+     let result=await  bcryptjs.compare(userCredObj.password,userInDB.password)
+     //if passwords not matched
+     if(result===false){
+      res.send({message:"Invalid password"})
+     }//if passwords are matched
+     else{
+      //create JWT token
+      let signedToken=jwt.sign({username:userInDB.username},'abcdef',{expiresIn:'1d'})
+      //send res
+      res.send({message:"login success",token:signedToken,payload:userInDB})
+     }
+    }
 }))
 
 
+
+//update user
+userApp.put(
+  "/user",
+  expressAsyncHandler(async (req, res) => {
+    //get modified user from req
+    const modifiedUser = req.body;
+    //update
+    // let updatedDoc = await UserModel.findOneAndUpdate(
+    //   { name: modifiedUser.name },
+    //   { $set: { ...modifiedUser } },
+    //   { returnOriginal: false }
+    // );
+    let updatedDoc = await UserModel.findByIdAndUpdate(
+      modifiedUser._id,
+      { $set: { ...modifiedUser } },
+      { returnOriginal: false }
+    );
+
+    //send res
+    res.send({ message: "updated user", payload: updatedDoc });
+  })
+);
+
+
+//delete user
+userApp.delete('/user/:_id',expressAsyncHandler(async(req,res)=>{
+    //get name from url
+   // const nameOfUrl=req.params.name;
+   const _id=req.params._id;
+    //delete
+    //let deletedUser=await UserModel.findOneAndDelete({name:nameOfUrl})
+    let deletedUser=await UserModel.findByIdAndDelete(_id)
+    //send res
+    res.send({messaeg:"user removed",payload:deletedUser})
+}))
+
 //export userApp
-module.exports=userApp;
+module.exports = userApp;
+
+
+//encryption(reversable)
+//hashing (irreverable)
---
+++ models/userModel.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+//create userSChema (username,password,city & age)
+const userSchema = new mongoose.Schema(
+  {
+    username:{
+      type:String,
+      required:true,
+      unique:true
+    },
+    password:{
+      type:String,
+      required:true,
+    },
+    city:{
+      type:String,
+      required:true,
+    },
+    age:{
+      type:Number,
+      required:true,
+      min:18,
+      max:60
+    }
+
+  },   
+  { strict: "throw" }
+);
+
+//create model for userSchema
+const UserModel = mongoose.model("user", userSchema);
+
+//export
+module.exports = UserModel;
